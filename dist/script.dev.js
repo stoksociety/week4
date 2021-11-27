ed7226936b26f1b30ba772838188d031a604e6eb
stocksociety <arnold.kagunda@student.moringaschool.com>
2021-11-27 22:43:18 +0300

    add the changes

---
+++ dist/script.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+varfunction = getPizza(name, size, topping, crust, total);
+{
+  (void 0).name = name;
+  (void 0).size = size;
+  (void 0).topping = topping;
+  This.crust = crust;
+  (void 0).total = total;
+}
